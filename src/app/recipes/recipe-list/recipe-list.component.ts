4816973594b8bd59b4273ce5b69ee42878315307
majdule <majstorovicdule@gmail.com>
2018-08-12 11:11:00 +0200

    Adding data from the model with string interpolation

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,9 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     // tslint:disable-next-line:max-line-length
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=aWyDp3CA')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=aWyDp3CA'),
+    // tslint:disable-next-line:max-line-length
+    new Recipe('Italian Pizza', 'Like the rest of Italian food, Italian pizza is best—and most authentic—when it’s made with fresh, local ingredients, especially any that are DOP. We’re not talking the microwaved dough and synthetic cheese that you see now both in Italy and abroad, but something completely different.', 'https://i1.wp.com/www.walksofitaly.com/blog/wp-content/uploads/2013/01/DSC_0021.jpg?resize=300%2C199&ssl=1')
   ];
   constructor() { }
 
