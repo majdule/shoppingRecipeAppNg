6b3959d342af493c67bd120f863e7a91f8da0c43
MAJ1BG <dusan.majstorovic@bosch.com>
2018-08-12 10:33:42 +0200

    Editing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeListItemComponent } from './recipes/recipe-list/recipe-list-item/recipe-list-item.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { HeaderComponent } from './header/header.component';
@@ -15,6 +16,7 @@ import { HeaderComponent } from './header/header.component';
     ShoppingListComponent,
     ShoppingListEditComponent,
     RecipeListComponent,
+    RecipeListItemComponent,
     RecipesComponent,
     RecipeDetailComponent,
     HeaderComponent
